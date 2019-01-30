94fcd6352bfccee14496d793700c750b36e94528
J-C-O <47179422+J-C-O@users.noreply.github.com>
2019-01-30 20:25:24 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express'); 
+var app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/', function (req, res) { // Routing to /
+res.send('Hallo! Dies ist der Heroku Test, sie haben ihn bestanden '); })
+
+app.get('/info', function (req, res) { // Routing to /info
+res.send('Hallo! Hier finden sie infos über die Gruppe 09'); })
+
+
+var server = app.listen(PORT, function () {
+var host = server.address().address
+var port = server.address().port
+console.log("Example app listening at http://%s:%s", host, port)
+}) // Achtung: Die Routings werden sequentiell geprüft und
+// bei Erfolg wird die Suche beendet !
